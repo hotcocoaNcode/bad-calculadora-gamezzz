1f9ba392000dcc20972a3dce55810e88e4ec7de0
coderboy <3picdestroyer@gmail.com>
2022-10-05 22:18:52 -0500

    AAAAAAAAAAAAAAA
    so damn close yet so damn farrr

--- src/main.c
+++ src/main.c
@@ -1,27 +1,55 @@
 #include <ti/getcsc.h>
 #include <graphx.h>
+#include <keypadc.h>
 
 int ballpx = GFX_LCD_WIDTH / 2;
 int ballpy = GFX_LCD_HEIGHT / 2;
 int ballxmod = 2;
 int ballymod = 1;
+int paddley = GFX_LCD_HEIGHT / 2;
+bool k_up, prevk_up, k_dw, prevk_dw = false;
+bool ccd = false;
 
 void draw()
 {
+
     gfx_SetColor(0);
-    gfx_FillRectangle_NoClip(0, 0, GFX_LCD_WIDTH, GFX_LCD_HEIGHT);
-    gfx_SetColor(255);
-    gfx_FillRectangle(ballpx, ballpy, 10, 10);
+    gfx_FillRectangle_NoClip(0, 0, GFX_LCD_WIDTH, GFX_LCD_HEIGHT); // overdocumenting
+    if (ballpy + 30 < paddley || ballpy + 10 > paddley){
+        gfx_SetColor(31);
+    }else{gfx_SetColor(255);}// wow really wonder what this does almost as if it sets the color to a number of 255 which seems to be white
+     
+    gfx_FillRectangle(ballpx, ballpy, 10, 10); // ball
+    gfx_FillRectangle(40, paddley, 5, 30); // human paddle
+    gfx_FillRectangle(GFX_LCD_WIDTH - 40, ballpy - 15, 5, 30); // ai paddle
 
 };
 void gametick() {
-    if (ballpx + 10 < GFX_LCD_WIDTH && ballpx > 0) {
-        ballpx = ballxmod + ballpx;
+    if (ballpx + 10 < GFX_LCD_WIDTH - 40 && ballpx > 0) {
+        if (ballpx > 45)
+        {
+           ballpx = ballxmod + ballpx;
+
+        }else{
+
+           if (ballpy + 30 > paddley || ballpy + 10 < paddley){
+
+                ballpx = ballxmod + ballpx;
+
+           }else{
+
+                ballxmod = ballxmod * -1;
+
+                ballpx = ballxmod + ballpx;
+
+           }
+        }
     }
     else {
         ballxmod = ballxmod * -1;
         ballpx = ballxmod + ballpx;
     }
+
     if (ballpy + 10 < GFX_LCD_HEIGHT && ballpy > 0) {
         ballpy = ballymod + ballpy;
     }
@@ -30,71 +58,69 @@ void gametick() {
         ballpy = ballymod + ballpy;
     }
 
+    if (paddley + 30 < GFX_LCD_HEIGHT){
+        if (k_dw){
+            paddley = paddley + 1;
+        }
+    }
+
+    if (paddley > 0){
+        if (k_up){
+            paddley = paddley - 1;
+        }
+    }
 };
+
+void kctick(){ 
+//because am dumbass and dont understando bit arrays n shit i'm using the getcsc method which slows the normally 60fps program to a grueling 10fps. 
+//using this method for keydetect because it'll get called once every 5 frames instead of every frame, hopefully doable now?
+
+// DUNCE CAP WORTHY ^^^^^^^
+// future me here, just steal from docs it still lagged like hell
+
+// DUNCE CAP WORTHY ^^^^^^^
+// future me here, just steal from docs but smarterly it took like 2 minutes to register one button press
+
+// DUNCE CAP WORTHY ^^^^^^^
+// future me here, just read the fucking docs.
+
+    kb_Scan();
+    
+    if (kb_Data[7] & kb_Up) {
+    k_up = true;
+    }else{
+    k_up = false;
+    }
+
+    if (kb_Data[7] & kb_Down) {
+    k_dw = true;
+    }else{
+    k_dw = false;
+    }
+
+    if (kb_Data[6] & kb_Clear) {
+    ccd = true;
+    }
+}
 
 int main(void)
 {
-    //uint8_t c;
-
-
-    /* Initialize graphics drawing */
-    
     gfx_Begin();
-    //new code
     gfx_SetDrawBuffer();
-    while (!os_GetCSC()) {
+    //int c = 5;
+
+    while (!ccd) {
         draw();
         gfx_SwapDraw();
         gametick();
+        //if (c == 5){
+        kctick();
+        //    c = 0;
+        //}else{
+        //    c = c+1;
+        //}
     }
 
-    //OLD CODE (USE FOR REFRENCE)
-    /*
-    //Draw to buffer instead of directly to screen
-    //This is the same as gfx_SetDraw(gfx_buffer)
-    gfx_SetDrawBuffer();
-
-    // Draw a gradient (to the buffer)
-    for (c = 0; c < 8; c++)
-    {
-        gfx_SetColor(c);
-        gfx_FillRectangle_NoClip(c * (GFX_LCD_WIDTH / 8), 0, GFX_LCD_WIDTH / 8, GFX_LCD_HEIGHT);
-    }
-
-    //Waits for a key
-    while (!os_GetCSC());
-
-    // Copy part of the drawn gradient to the visible screen
-    gfx_CopyRectangle(gfx_buffer, gfx_screen, GFX_LCD_WIDTH / 4, GFX_LCD_HEIGHT / 4, GFX_LCD_WIDTH / 4, GFX_LCD_HEIGHT / 4, GFX_LCD_WIDTH / 2, GFX_LCD_HEIGHT / 2);
-
-    // Waits for a key
-    while (!os_GetCSC());
-
-    // This is the same as gfx_SetDraw(gfx_screen)
-    gfx_SetDrawScreen();
-
-    // Draw a gradient (to the screen)
-    for (c = 16; c < 24; c++)
-    {
-        gfx_SetColor(c);
-        gfx_FillRectangle_NoClip((c - 16) * (GFX_LCD_WIDTH / 8), 0, GFX_LCD_WIDTH / 8, GFX_LCD_HEIGHT);
-    }
-
-    // Copy part of the drawn gradient to the offscreen buffer
-    gfx_CopyRectangle(gfx_screen, gfx_buffer, GFX_LCD_WIDTH / 4, GFX_LCD_HEIGHT / 4, GFX_LCD_WIDTH / 4, GFX_LCD_HEIGHT / 4, GFX_LCD_WIDTH / 2, GFX_LCD_HEIGHT / 2);
-
-    // Waits for a key
-    while (!os_GetCSC());
-
-    // Copy the offscreen buffer to the visible screen 
-    // Same as gfx_BlitBuffer()
-    gfx_CopyRectangle(gfx_buffer, gfx_screen, 0, 0, 0, 0, GFX_LCD_WIDTH, GFX_LCD_HEIGHT);
-
-    // Waits for a key
-    while (!os_GetCSC());
-    */
-
-    /* End graphics drawing */
     gfx_End();
 
     return 0;
