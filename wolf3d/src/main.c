cb2becd995dedf35f30ff06faf9c90c008214de3
coderboy <95043086+hotcocoaNcode@users.noreply.github.com>
2022-10-09 11:44:05 -0500

    rays casted

--- wolf3d/src/main.c
+++ wolf3d/src/main.c
@@ -7,11 +7,13 @@
 bool k_up, k_dw, k_lf, k_rg = false;
 bool ccd = false;
 bool lost = false;
-float pi = 3.14159;
+const float pi = 3.14159;
+const float pi2 = pi / 2;
+const float pi3 = 3 * pi / 2;
 
-float px, py;
+float px, py, pdx, pdy, pa;
 
-// 4:51 in video 1
+// 11:44 in video 1
 
 // 320x240 -> 106x48
 // Much MUCH more managable for graphics...
@@ -20,7 +22,12 @@ int SCWIDTH = 106;
 int SCHEIGHT = 48;
 int SCALEFACTOR = 5;
 
-int mapX = 8, mapY = 8, mapS = 10;
+void drawPixelScale5(int x, int y, int c) {
+    gfx_SetColor(c);
+    gfx_FillRectangle(x*5, y*5, 5, 5);
+}
+
+int mapX = 8, mapY = 8, mapS = 32;
 int map[] =
 {
  1,1,1,1,1,1,1,1,
@@ -33,16 +40,107 @@ int map[] =
  1,1,1,1,1,1,1,1,
 };
 
-void drawPixel(int x, int y, int c) {
-    gfx_SetColor(c);
-    gfx_FillRectangle(x*5 + 2, y*5 + 2, 5, 5);
+float dist(float ax, float ay, float bx, float by, float ang) {
+    //PYTAGORAS TEROEM
+    //WEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEE
+    return sqrt((bx - ax) * (bx - ax) + (by - ay) * (by - ay));
+}
+
+void drawRays3D() {
+    int r, mx, my, mp, dof;
+    float rx, ry, ra, xo, yo;
+
+
+    //quick scaling fix
+    float usePX = px * 2, usePY = py * 2;
+    
+
+    ra = pa;
+    for (r = 0; r < 1; r++) {
+        
+        
+        //HORIZONTAL CHECK
+        dof = 0;
+        float aTan = -1 / tan(ra);
+
+        if (ra>pi) { //up
+            ry = (((int)usePY >> 6) << 6) - 0.0001;
+            rx = (usePY - ry) * aTan + usePX;
+            yo = -64;
+            xo = -yo * aTan;
+        }
+        if (ra < pi) { //down
+            ry = (((int)usePY >> 6) << 6) + 64;
+            rx = (usePY - ry) * aTan + usePX;
+            yo = 64;
+            xo = -yo * aTan;
+        }
+        if (ra == 0 || ra == pi) { // left or right exactly
+            rx = usePX;
+            ry = usePY;
+            dof = 8;
+        }
+        while (dof < 8) {
+            mx = (int)(rx) >> 6;
+            my = (int)(ry) >> 6;
+            mp = my * mapX + mx;
+            if (mp > 0 && mp < mapX * mapY && map[mp] == 1) {
+                dof = 8;
+            }
+            else {
+                rx += xo;
+                ry += yo;
+                dof += 1;
+            }
+        }
+        gfx_SetColor(103);
+        gfx_Line(usePX/2, usePY / 2, rx/2, ry/2);
+        
+
+        //VERTICAL CHECK
+        dof = 0;
+        float nTan = -tan(ra);
+
+        if (ra > pi2 && ra<pi3) { //right
+            rx = (((int)usePX >> 6) << 6) - 0.0001;
+            ry = (usePX - rx) * nTan + usePY;
+            xo = -64;
+            yo = -xo * nTan;
+        }
+        if (ra < pi2 || ra > pi3) { //left
+            rx = (((int)usePX >> 6) << 6) + 64;
+            ry = (usePX - rx) * nTan + usePY;
+            xo = 64;
+            yo = -xo * nTan;
+        }
+        if (ra == 0 || ra == pi) { //up or down exactly
+            ry = usePY;
+            rx = usePX;
+            dof = 8;
+        }
+        while (dof < 8) {
+            my = (int)(ry) >> 6;
+            mx = (int)(rx) >> 6;
+            mp = mx * mapY + my;
+            if (mp > 0 && mp < mapY * mapX && map[mp] == 1) {
+                dof = 8;
+            }
+            else {
+                ry += yo;
+                rx += xo;
+                dof += 1;
+            }
+        }
+        gfx_SetColor(224);
+        gfx_Line(usePX / 2, usePY / 2, rx / 2, ry / 2);
+    }
 }
 
 void drawMap2D()
 {
     int x, y, xo, yo;
-    for (int y = 0; y < mapY; y++) {
-        for (int x = 0; x < mapX; x++) {
+    for (y = 0; y < mapY; y++) {
+        for (x = 0; x < mapX; x++) {
             if (map[y*mapX+x]==1)
             {
                 gfx_SetColor(255);
@@ -50,36 +148,55 @@ void drawMap2D()
             else {
                 gfx_SetColor(0);
             }
-            xo = x * mapS;
-            yo = y * mapS;
-            gfx_FillRectangle(xo, yo, mapS, mapS);
+            xo = x * (mapS);
+            yo = y * (mapS);
+            gfx_FillRectangle(xo+1, yo+1, mapS-1, mapS-1);
         }
     }
 }
 
+void drawPlayer() {
+    gfx_SetColor(231);
+    gfx_FillRectangle(px-2, py-2, 5, 5);
+}
+
 void draw()
 {
-    gfx_FillScreen(9);
+    gfx_FillScreen(74);
     drawMap2D();
+    drawPlayer();
+    drawRays3D();
 };
 
 void gametick() {
 
     //movement
     if (k_lf) {
-        px -= 3;
+        pa -= 0.1;
+        if (pa < 0) {
+            pa += 2 * pi;
+        }
+        pdx = cos(pa)*5;
+        pdy = sin(pa)*5;
     }
 
     if (k_rg) {
-        px += 3;
+        pa += 0.1;
+        if (pa > 2*pi) {
+            pa -= 2 * pi;
+        }
+        pdx = cos(pa) * 5;
+        pdy = sin(pa) * 5;
     }
 
     if (k_up) {
-        py -= 3;
+        px += pdx;
+        py += pdy;
     }
 
     if (k_dw) {
-        py += 3;
+        px -= pdx;
+        py -= pdy;
     }
 };
 
@@ -120,8 +237,11 @@ void kctick(){
 void init() {
     gfx_Begin();
     gfx_SetDrawBuffer();
-    px = 300;
-    py = 300;
+    px = 30;
+    py = 30;
+    pa = 0;
+    pdx = cos(pa) * 5;
+    pdy = sin(pa) * 5;
 }
 
 int main()
